a1316e2059278e257d90d1b6dcb8337fccb219da
1997priyam <1997priyam@gmail.com>
2020-01-07 14:00:43 +0530

    Changed the location of the file to correct location

--- Disjoint sets/codearena2.cpp
+++ Disjoint sets/codearena2.cpp
@@ -39,7 +39,7 @@ Here, the two campers find each other, so the answer is 0.
 
 using namespace std;
 
-class element{
+class element{              // Class which represents an element of the Disjoint set
     public:
         int rank;
         int size;
@@ -52,74 +52,74 @@ class element{
 };
 
 int find(vector<element> &set, int person){
-    if(set[person].parent != person){
-        set[person].parent = find(set, set[person].parent);
+    if(set[person].parent != person){                       // If the parent is not equal to root 
+        set[person].parent = find(set, set[person].parent);  // find the parent of that node until the root is found and hence it will be the ID.
     }
-    return set[person].parent;
+    return set[person].parent;          // Return the updated parent
 }
 
 
 bool merge_group(vector<element> &set, int source, int dest){
-    int id1 = find(set, source);
+    int id1 = find(set, source);    // Finding ID of both source and destination.
     int id2 = find(set, dest);
-    if(id1==id2){
-        return false;
+    if(id1==id2){               // If the IDs are equal no need to merge as they are already in same group.
+        return false;          //  Return false since the group is not merged.
     }
     else{
-        int rank1 = set[id1].rank;
+        int rank1 = set[id1].rank;      // Finding the ranks of the parents of source and destination 
         int rank2 = set[id2].rank;
         
-        if(rank1>rank2){
-            set[id2].parent = id1;
-            set[id1].size += set[id2].size;
-            set[id2].size = 0;
+        if(rank1>rank2){            // Merging under the one having greater rank
+            set[id2].parent = id1;      // Changing the parent 
+            set[id1].size += set[id2].size; // Increasing the size of the new group by adding present size of groups
+            set[id2].size = 0;          // Resetting the size of group to 0 / optional
         }
         else{
-            set[id1].parent = id2;
+            set[id1].parent = id2;      // Vice versa of the above process
             set[id2].size += set[id1].size;
             set[id1].size = 0;
-            if(rank1==rank2){
+            if(rank1==rank2){       // If the ranks are equal merge anhy group under any and increase the rank 
                 set[id2].rank++;
             }
         }
-        return true;
+        return true;        // Returning true since the actual merge happened here.
     }
 }
 
 int get_difference(vector<element> &set){
     int smallest = INT32_MAX;
     int largest = INT32_MIN;
-    for(int i=0; i<set.size(); i++){
+    for(int i=0; i<set.size(); i++){       // Finding the smallest and the largest group sizes
         int id = find(set, i);
         int size = set[id].size;
         if(size>largest) largest = size;
         if(size<smallest) smallest = size;
     }
-    return largest - smallest;
+    return largest - smallest;      // Returning the difference as requested.
 }
 
 int main(){
     int N, Q;
     cin>>N>>Q;
-    vector<element> set;
+    vector<element> set;    
     for(int i=0; i<N; i++){
-        set.push_back(element(1,i,0));
+        set.push_back(element(1,i,0));  // Pushing each person as a group in itself with size-1 , parent- itself, and rank-0
     }
     int source, destination;
-    int total_groups = N;
-    bool flag = true;
+    int total_groups = N;               // Variable to keep track if total groups is 1. Then return 0.
+    bool flag = true;                   // Flag to check if there is 1 group then print 0. and don't calculate the distance.
     for(int i=0; i<Q; i++){
         cin>>source>>destination;
-        if(merge_group(set, source-1, destination-1)){
-            total_groups--;
-            if(total_groups==1){
+        if(merge_group(set, source-1, destination-1)){  // Taking the source and destination and merging them in the group
+            total_groups--;                         // Subtracting 1 group if merge is successful.
+            if(total_groups==1){            // If remaining group is 1 then print 0 and exit.
                 cout<<0;
                 flag = false;
                 break;
             }
         }
     }
-    if(flag){
+    if(flag){                           // If the total groups are greater than 1 then calculate the difference b/w largest and smallest as requested.
         cout<<get_difference(set);
     }
     return 0;
