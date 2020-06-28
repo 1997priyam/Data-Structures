87bcd978353e2d9e1737b2909dc85b5d18004e68
1997priyam <1997priyam@gmail.com>
2020-06-28 21:24:21 +0530

    sorted array to BST leetcode -> Done

---
+++ trees/sortedArrayToBST.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBSTUtil(nums, 0, nums.size() - 1);
+    }
+    
+    TreeNode* sortedArrayToBSTUtil(vector<int> &nums, int start, int end) {
+        if (start > end) return NULL;
+        int mid = (start + end) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        
+        root->left = sortedArrayToBSTUtil(nums, start, mid - 1);
+        root->right = sortedArrayToBSTUtil(nums, mid + 1, end);
+        
+        return root;
+    }
+};
