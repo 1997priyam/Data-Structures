306f4abe6ce3c19e73fd6077804c896640ab98b8
1997priyam <1997priyam@gmail.com>
2020-07-05 16:15:48 +0530

    array contains any duplicates or not - leetcode

---
+++ arrays&mix/conatins_duplicates.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if(nums.size() == 0) return false;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-1; i++) {
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
+    
+};
