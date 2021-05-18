46cf496c9f0ec22cf316409719515453bb46a640
1997priyam <1997priyam@gmail.com>
2021-05-18 11:55:07 +0530

    merge two list minor change

--- LinkedList/merge_2_sorted_ll.cpp
+++ LinkedList/merge_2_sorted_ll.cpp
@@ -4,7 +4,7 @@ using namespace std;
   struct ListNode {
       int val;
       ListNode *next;
-    //   ListNode(int x) : val(x), next(NULL) {}
+      ListNode(int x) : val(x), next(NULL) {}
   };
  
 class Solution {
