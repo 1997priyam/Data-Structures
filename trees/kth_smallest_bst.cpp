b580e7ee946df0e510059f2120d21c3e2ed25771
1997priyam <1997priyam@gmail.com>
2021-06-09 22:31:35 +0530

    kth smmallest element bst

---
+++ trees/kth_smallest_bst.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> arr;
+        inorder(root, arr);
+        return arr[k-1];
+    }
+    
+    void inorder(TreeNode* root, vector<int> &ans) {
+        if(!root) return;
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+};
